cc38c688eb6b29892599cef27371b629bd2645a6
doge <15705817983@163.com>
2018-07-16 23:44:46 +0800

    添加节点

---
+++ untitled1/src/demo3/RBTree.java
@@ -0,0 +1,24 @@
+package demo3;
+
+public class RBTree<T extends Comparable<T>> {
+    private RBTNode<T> mRoot;    // 根结点
+    private static final boolean RED = false;
+    private static final boolean BLACK = true;
+
+    public class RBTNode<T extends Comparable<T>> {
+        boolean color;
+        T key;
+        RBTNode<T> left;    // 左孩子
+        RBTNode<T> right;    // 右孩子
+        RBTNode<T> parent;    // 父结点
+
+
+        public RBTNode(boolean color, T key, RBTNode<T> left, RBTNode<T> right, RBTNode<T> parent) {
+            this.color = color;
+            this.key = key;
+            this.left = left;
+            this.right = right;
+            this.parent = parent;
+        }
+    }
+}
